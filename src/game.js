3dc6fc062c82c4a0c1b25856f689c23d5f209d38
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-04-03 16:47:57 -0400

    remove canvas resizing

--- src/game.js
+++ src/game.js
@@ -25,19 +25,18 @@ import { Lvl_1 } from './levels/lvl1'
 let gameConfig = {
   type: Phaser.AUTO,
   parent: 'canvas',
-  width: deviceW,
-  height: deviceH,
+  width: 800,
+  height: 650,
   physics: {
       default: 'arcade',
       arcade: {
-        gravity: { y: 0, x: 0 },
+        gravity: { y: 0},
         debug: true
       }
     },
     scene: [
       Engineering
-    ],
-    resize: resize
+    ]
 }
 
 
@@ -45,27 +44,27 @@ let config = gameConfig;
 
 let game = new Phaser.Game(config);
 
-function resize() {
-		var w = window.innerWidth;
-		var h = window.innerHeight;
-		var scale = Math.min(w / Config.DEFAULT_WIDTH, h / Config.DEFAULT_HEIGHT);
+// function resize() {
+// 		var w = window.innerWidth;
+// 		var h = window.innerHeight;
+// 		var scale = Math.min(w / Config.DEFAULT_WIDTH, h / Config.DEFAULT_HEIGHT);
 
-		game.canvas.setAttribute('style',
-			' -ms-transform: scale(' + scale + '); -webkit-transform: scale3d(' + scale + ', 1);' +
-			' -moz-transform: scale(' + scale + '); -o-transform: scale(' + scale + '); transform: scale(' + scale + ');' +
-			' transform-origin: top left;'
-		);
+// 		game.canvas.setAttribute('style',
+// 			' -ms-transform: scale(' + scale + '); -webkit-transform: scale3d(' + scale + ', 1);' +
+// 			' -moz-transform: scale(' + scale + '); -o-transform: scale(' + scale + '); transform: scale(' + scale + ');' +
+// 			' transform-origin: top left;'
+// 		);
 
-		width = w / scale;
-		height = h / scale;
-		game.resize(width, height);
-		game.scene.scenes.forEach(function (scene) {
-			scene.cameras.main.setViewport(0, 0, width, height);
-		});
-	}
+	// 	width = w / scale;
+	// 	height = h / scale;
+	// 	game.resize(width, height);
+	// 	game.scene.scenes.forEach(function (scene) {
+	// 		scene.cameras.main.setViewport(0, 0, width, height);
+	// 	});
+	// }
 
-window.addEventListener('resize', function (event) {
-     console.log("RESIZE EVENT ");
-     if(game.isBooted) resize();
-     else game.events.once('boot', resize);
-}, false);
+// window.addEventListener('resize', function (event) {
+//      console.log("RESIZE EVENT ");
+//      if(game.isBooted) resize();
+//      else game.events.once('boot', resize);
+// }, false);
